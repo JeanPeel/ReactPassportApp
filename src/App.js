dc7858f5bd870bac7a122dd3f6667742eb0a3419
Jean Peel <Jean.Peel.246@gmail.com>
2020-07-16 03:35:15 -0600

    setting up react pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import logo from './logo.svg';
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Blog from "./pages/Blog";
+import Connect from "./pages/Connect";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/connect" component={Connect} />
+      </div>
+    </Router>
   );
 }
 
